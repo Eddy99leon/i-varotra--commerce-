0d167e1ef5382df93c01145019566fa96a70d15f
Eddy Léon <leoneddy504@gmail.com>
2023-09-01 10:28:46 +0200

    Detail Sidebar

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,24 +2,28 @@ import React, { useContext } from "react"
 import { Link } from "react-router-dom";
 import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io"
 import { CartContext } from "../contexts/CartContext";
+import { SidebarContext } from "../contexts/SidebarContext"
 
 
 const CartItem = ({item}) => {
   const { id, title, image, price, amount } = item ;
   const { removeFromCart, increaseAmount, decreaseAmount } = useContext(CartContext);
+  const { setIsOpen } = useContext(SidebarContext)
   
   return (
     <div className="flex py-2 border-b-2">
 
       <div className="w-full min-h-[150px] flex items-center gap-x-4">
 
-        <Link to={`/product/${id}`}>
+        <Link onClick={() => setIsOpen(false)} to={`/product/${id}`}>
           <img className="max-w-[80px]" src={image} alt="" />
         </Link>
 
         <div className="flex flex-col w-full ml-2">
           <div className="flex items-center justify-between mb-2">
-            <Link to={`/product/${id}`} className="text-sm font-semibold">{title}</Link>
+            <Link onClick={() => setIsOpen(false)} to={`/product/${id}`} className="text-sm font-semibold">
+              {title}
+            </Link>
             <div>
               <IoMdClose onClick={() => removeFromCart(id)} className="text-xl text-gray-500 transition cursor-pointer hover:text-black" />
             </div>
